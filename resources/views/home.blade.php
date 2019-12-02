52922b5a1a91ef2ad13fb4b0ef4c8d0869d65ef7
Firmansyah Yanuar <fyfirman@gmail.com>
2019-12-02 18:09:53 +0700

    Add SweetAlert

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,13 +5,17 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" type="text/css" href="{{ asset('css/style.css') }}">
+    <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
     <title>Document</title>
 </head>
 <body>
     <!-- <header id="header">
         
     </header> -->
-
+    <script>
+        if(true) //TODO: ganti sama is login success
+            swal('Login Sukses!', '', 'success');
+    </script>
     <div class="home">
         <nav>
             <div class="logo">
